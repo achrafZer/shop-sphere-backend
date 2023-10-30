0173d2636ccca4d478e01f5e61e57ccf1c9610af
Achraf <zerhouniachraf@hotmail.com>
2023-10-30 21:25:12 +0100

    feat : AdminRepository DONE

--- src/main/java/com/shop/sphere/dao/AdminRepository.java
+++ src/main/java/com/shop/sphere/dao/AdminRepository.java
@@ -1,4 +1,15 @@
 package com.shop.sphere.dao;
 
-public class AdminRepository {
+import com.shop.sphere.models.Admin;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repository interface for managing {@link Admin} entities.
+ * <p>
+ * Provides CRUD operations on Admin entities.
+ * </p>
+ */
+@Repository
+public interface AdminRepository extends CrudRepository<Admin, Long> {
 }
