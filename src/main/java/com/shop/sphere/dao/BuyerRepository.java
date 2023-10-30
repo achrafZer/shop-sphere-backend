112f31485867354e54cb6e4ba5e7940159da3c60
Achraf <zerhouniachraf@hotmail.com>
2023-10-30 21:25:55 +0100

    feat : BuyerRepository DONE

--- src/main/java/com/shop/sphere/dao/BuyerRepository.java
+++ src/main/java/com/shop/sphere/dao/BuyerRepository.java
@@ -1,4 +1,15 @@
 package com.shop.sphere.dao;
 
-public class BuyerRepository {
+import com.shop.sphere.models.Buyer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repository interface for managing {@link Buyer} entities.
+ * <p>
+ * Provides CRUD operations on Buyer entities.
+ * </p>
+ */
+@Repository
+public interface BuyerRepository extends CrudRepository<Buyer, Long> {
 }
