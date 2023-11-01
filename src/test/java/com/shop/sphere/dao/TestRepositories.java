bd3a4ac837fde7ff917f70138bbd11f4a5752300
Achraf <zerhouniachraf@hotmail.com>
2023-11-01 18:53:09 +0100

    test : test update-buyer repo

--- src/test/java/com/shop/sphere/dao/TestRepositories.java
+++ src/test/java/com/shop/sphere/dao/TestRepositories.java
@@ -39,4 +39,36 @@ public class TestRepositories {
         assertThat(foundBuyer.getPassword()).isEqualTo(buyer.getPassword());
         assertThat(foundBuyer.getAddress()).isEqualTo(buyer.getAddress());
     }
+
+    @Test
+    public void testBuyerUpdate() {
+        // 1. Create and save an initial buyer (Juba OUARAB)
+        Buyer buyer = new Buyer();
+        buyer.setFirstName("Juba");
+        buyer.setLastName("OUARAB");
+        buyer.setEmail("juba.ouarab@shopsphere.com");
+        buyer.setPassword("jubaPassword");
+        buyer.setAddress("456 Avenue Juba");
+        Buyer savedBuyer = buyerRepository.save(buyer);
+        entityManager.flush();
+
+        // 2. Update the saved buyer's attributes
+        savedBuyer.setFirstName("Kpotivi");
+        savedBuyer.setLastName("KPOTY");
+        savedBuyer.setEmail("kpotivi.kpoty@shopsphere.com");
+        savedBuyer.setPassword("kpotiviPassword");
+        savedBuyer.setAddress("789 Street Kpotivi");
+        Buyer updatedBuyer = buyerRepository.save(savedBuyer);
+        entityManager.flush();
+
+        // 3. Fetch the updated buyer from the database and verify the changes
+        Buyer foundBuyer = entityManager.find(Buyer.class, updatedBuyer.getId());
+
+        assertThat(foundBuyer).isNotNull();
+        assertThat(foundBuyer.getFirstName()).isEqualTo("Kpotivi");
+        assertThat(foundBuyer.getLastName()).isEqualTo("KPOTY");
+        assertThat(foundBuyer.getEmail()).isEqualTo("kpotivi.kpoty@shopsphere.com");
+        assertThat(foundBuyer.getPassword()).isEqualTo("kpotiviPassword");
+        assertThat(foundBuyer.getAddress()).isEqualTo("789 Street Kpotivi");
+    }
 }
