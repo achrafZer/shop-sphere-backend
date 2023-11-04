73397c29a72cef9c74a37ae983a7e1ef8ce9adda
Achraf <zerhouniachraf@hotmail.com>
2023-11-04 14:56:08 +0100

    feat : add mapper for buyer

--- src/main/java/com/shop/sphere/mappers/BuyerMapper.java
+++ src/main/java/com/shop/sphere/mappers/BuyerMapper.java
@@ -1,2 +1,13 @@
-package com.shop.sphere.mappers;public interface BuyerMapper {
+package com.shop.sphere.mappers;
+
+import com.shop.sphere.api.model.BuyerDTO;
+import com.shop.sphere.models.Buyer;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface BuyerMapper {
+
+    BuyerDTO buyerToBuyerDto(Buyer buyer);
+
+    Buyer buyerDtotoBuyer(BuyerDTO buyerDTO);
 }
