f5acf6afeca4a70e0279a9086367a86fc90cebb4
Achraf <zerhouniachraf@hotmail.com>
2023-11-04 14:57:43 +0100

    feat : add create buyer endpoint

--- src/main/java/com/shop/sphere/controllers/BuyerRestController.java
+++ src/main/java/com/shop/sphere/controllers/BuyerRestController.java
@@ -3,16 +3,36 @@ package com.shop.sphere.controllers;
 import com.shop.sphere.api.BuyersApi;
 import com.shop.sphere.api.model.BuyerDTO;
 import com.shop.sphere.api.model.IdBuyer;
+import com.shop.sphere.dao.BuyerRepository;
+import com.shop.sphere.mappers.AdminMapper;
+import com.shop.sphere.mappers.BuyerMapper;
+import com.shop.sphere.models.Buyer;
+import io.swagger.annotations.Authorization;
+import org.mapstruct.factory.Mappers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 public class BuyerRestController implements BuyersApi {
+
+    @Autowired
+    private BuyerRepository buyerRepository;
+
+    private BuyerMapper buyerMapper = Mappers.getMapper(BuyerMapper.class);
+
     @Override
-    public ResponseEntity<IdBuyer> createBuyer(BuyerDTO buyerDTO) {
-        return BuyersApi.super.createBuyer(buyerDTO);
+    public ResponseEntity<IdBuyer> createBuyer(@Valid @RequestBody BuyerDTO buyerDTO) {
+        Buyer buyer = buyerMapper.buyerDtotoBuyer(buyerDTO);
+        Buyer savedBuyer = buyerRepository.save(buyer);
+        IdBuyer idBuyer = new IdBuyer();
+        idBuyer.setIdBuyer(savedBuyer.getId());
+        return new ResponseEntity<>(idBuyer, HttpStatus.CREATED);
     }
 
     @Override
