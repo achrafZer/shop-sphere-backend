d22091ed952af3c65e7efcc9b8b8045d9cc73361
Achraf <zerhouniachraf@hotmail.com>
2023-11-06 21:20:52 +0100

    test : set up test order controller

--- src/test/java/com/shop/sphere/controllers/TestOrderController.java
+++ src/test/java/com/shop/sphere/controllers/TestOrderController.java
@@ -1,2 +1,80 @@
-package com.shop.sphere.controllers;public class TestOrderController {
+package com.shop.sphere.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.shop.sphere.api.model.OrderDTO;
+import com.shop.sphere.dao.OrderRepository;
+import com.shop.sphere.mappers.OrderMapper;
+import com.shop.sphere.models.Buyer;
+import com.shop.sphere.models.Order;
+import com.shop.sphere.models.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TestOrderController {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private OrderRepository orderRepository;
+
+    private OrderMapper orderMapper = Mappers.getMapper(OrderMapper.class);
+
+    private Order testOrder;
+    private OrderDTO testOrderDTO;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @BeforeEach
+    public void setUp() {
+        testOrder = new Order();
+        testOrder.setNumber(1L);
+        testOrder.setDate(LocalDate.now());
+        testOrder.setIsFinalized(true);
+
+        List<Product> articles = new ArrayList<>();
+
+        Product product = new Product();
+        product.setDescription("description test");
+        product.setId(1L);
+        product.setPhoto("https://fefef.fzfzfz.com");
+        product.setPrice(22.5);
+        product.setTitle("Titre");
+        product.setQuantity(85);
+
+        Product product2 = new Product();
+        product2.setDescription("description test for the second product");
+        product2.setId(1L);
+        product2.setPhoto("https://fefefezded.fzfzfz.com");
+        product2.setPrice(2.5);
+        product2.setTitle("second produit");
+        product2.setQuantity(85);
+
+        articles.add(product);
+        articles.add(product2);
+        testOrder.setArticles(articles);
+
+        Buyer client = new Buyer();
+        client.setId(1L);
+        client.setFirstName("ClientFirstName");
+        client.setLastName("ClientLastName");
+        client.setEmail("client.email@example.com");
+        client.setPassword("securePassword"); // For test purposes only; be careful with real passwords
+        client.setAddress("123 Client Address Street");
+        testOrder.setClient(client); // Assuming Order has a setClient method
+
+        testOrderDTO = orderMapper.orderToOrderDto(testOrder);
+    }
 }
