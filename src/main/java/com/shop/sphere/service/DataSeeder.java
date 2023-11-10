265d5ef115f9bfa2b377e374667078680e17899b
KPOTY Kpotivi <kpotivi.kpoty@etu.univ-amu.fr>
2023-11-10 21:36:40 +0100

    fix:fix order creation

--- src/main/java/com/shop/sphere/service/DataSeeder.java
+++ src/main/java/com/shop/sphere/service/DataSeeder.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 import javax.annotation.PostConstruct;
 
 @Service
-//@Profile("test")
+@Profile("test")
 public class DataSeeder {
 
     @Autowired
