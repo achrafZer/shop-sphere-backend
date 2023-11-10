dd89ad6de0d202fe990ec5b06f0560f0b6f36c79
Achraf <zerhouniachraf@hotmail.com>
2023-11-10 22:57:50 +0100

    test : order to dto

--- src/test/java/com/shop/sphere/mappers/OrderMapperTest.java
+++ src/test/java/com/shop/sphere/mappers/OrderMapperTest.java
@@ -1,8 +1,14 @@
 package com.shop.sphere.mappers;
 
+import com.shop.sphere.api.model.OrderDTO;
+import com.shop.sphere.models.Order;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class OrderMapperTest {
 
@@ -12,5 +18,17 @@ public class OrderMapperTest {
     @BeforeEach
     void setup() {
         orderMapper = Mappers.getMapper(OrderMapper.class);
+    }
+
+    @Test
+    void shouldMapOrderToOrderDto() {
+        LocalDate date = LocalDate.of(2023, 4, 1);
+        Order order = new Order();
+        order.setDate(date);
+
+        OrderDTO orderDTO = orderMapper.orderToOrderDto(order);
+
+        assertThat(orderDTO).isNotNull();
+        assertThat(orderDTO.getDate()).isEqualTo("01/04/2023");
     }
 }
