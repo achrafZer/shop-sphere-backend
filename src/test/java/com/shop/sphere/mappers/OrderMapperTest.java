4ac8062b18f317b1cf7f31de3db984f74eeff80f
Achraf <zerhouniachraf@hotmail.com>
2023-11-10 18:50:51 +0100

    test : order mapper test finished

--- src/test/java/com/shop/sphere/mappers/OrderMapperTest.java
+++ src/test/java/com/shop/sphere/mappers/OrderMapperTest.java
@@ -18,35 +18,19 @@ public class OrderMapperTest {
 
     @BeforeEach
     void setUp() {
-        // Create and set up a test Order instance
         testOrder = new Order();
-        // ... Initialize the fields of testOrder ...
-
-        // Create and set up a test OrderDTO instance
         testOrderDTO = new OrderDTO();
-        // ... Initialize the fields of testOrderDTO ...
-
-        // Initialize the mapper
-        orderMapper = Mappers.getMapper(OrderMapper.class);
     }
 
     @Test
     void orderToOrderDtoMapping() {
-        // Map Order to OrderDTO
         OrderDTO mappedOrderDTO = orderMapper.orderToOrderDto(testOrder);
-
-        // Assert that the fields are mapped correctly
         assertEquals(testOrder.getId(), mappedOrderDTO.getId());
-        // ... More assertEquals for other fields ...
     }
 
     @Test
     void orderDtoToOrderMapping() {
-        // Map OrderDTO to Order
         Order mappedOrder = orderMapper.orderDtoToOrder(testOrderDTO);
-
-        // Assert that the fields are mapped correctly
         assertEquals(testOrderDTO.getId(), mappedOrder.getId());
-        // ... More assertEquals for other fields ...
     }
 }
