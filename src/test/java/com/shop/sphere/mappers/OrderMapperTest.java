35c2fabf7e27f9841f1977e59859a4d58db9e9af
Achraf <zerhouniachraf@hotmail.com>
2023-11-10 22:55:53 +0100

    test : recreate setUp for order mapper test

--- src/test/java/com/shop/sphere/mappers/OrderMapperTest.java
+++ src/test/java/com/shop/sphere/mappers/OrderMapperTest.java
@@ -1,27 +1,16 @@
 package com.shop.sphere.mappers;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+
 public class OrderMapperTest {
 
-//    private OrderMapper orderMapper = Mappers.getMapper(OrderMapper.class);
-//
-//    private Order testOrder;
-//    private OrderDTO testOrderDTO;
-//
-//    @BeforeEach
-//    void setUp() {
-//        testOrder = new Order();
-//        testOrderDTO = new OrderDTO();
-//    }
-//
-//    @Test
-//    void orderToOrderDtoMapping() {
-//        OrderDTO mappedOrderDTO = orderMapper.orderToOrderDto(testOrder);
-//        assertEquals(testOrder.getId(), mappedOrderDTO.getId());
-//    }
-//
-//    @Test
-//    void orderDtoToOrderMapping() {
-//        Order mappedOrder = orderMapper.orderDtoToOrder(testOrderDTO);
-//        assertEquals(testOrderDTO.getId(), mappedOrder.getId());
-//    }
+    private OrderMapper orderMapper;
+
+    @Test
+    @BeforeEach
+    void setup() {
+        orderMapper = Mappers.getMapper(OrderMapper.class);
+    }
 }
