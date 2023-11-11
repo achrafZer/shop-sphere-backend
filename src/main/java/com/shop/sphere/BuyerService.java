113c702d7ec93d3ff5011f5b3688b5f2cc7e8966
Achraf <zerhouniachraf@hotmail.com>
2023-11-11 10:05:06 +0100

    feat : finish buyer service

--- src/main/java/com/shop/sphere/BuyerService.java
+++ src/main/java/com/shop/sphere/BuyerService.java
@@ -1,2 +1,23 @@
-package com.shop.sphere;public class BuyerService {
+package com.shop.sphere;
+
+import com.shop.sphere.dao.BuyerRepository;
+import com.shop.sphere.models.Buyer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BuyerService {
+
+    @Autowired
+    private BuyerRepository buyerRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public Buyer saveBuyer(Buyer buyer) {
+        String encodedPassword = passwordEncoder.encode(buyer.getPassword());
+        buyer.setPassword(encodedPassword);
+        return buyerRepository.save(buyer);
+    }
 }
