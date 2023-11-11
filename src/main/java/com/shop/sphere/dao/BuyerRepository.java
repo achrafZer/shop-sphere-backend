5b00715ffed119128a96c78083c08da9d691a360
Achraf <zerhouniachraf@hotmail.com>
2023-11-11 10:58:50 +0100

    feat : add find by email for buyer

--- src/main/java/com/shop/sphere/dao/BuyerRepository.java
+++ src/main/java/com/shop/sphere/dao/BuyerRepository.java
@@ -4,6 +4,8 @@ import com.shop.sphere.models.Buyer;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 /**
  * Repository interface for managing {@link Buyer} entities.
  * <p>
@@ -15,4 +17,7 @@ public interface BuyerRepository extends CrudRepository<Buyer, Long> {
 
     Buyer findBuyerByEmailAndPassword(String email, String password);
 
+    Buyer findBuyerByEmail(String email);
+
+    Optional<Buyer> findByEmail(String email);
 }
